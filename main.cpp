4b2713a603a4821a31af7bfc499d71e779d65718
cooper248 <coop248@gmail.com>
2017-04-03 19:56:14 -0700

    All initializations up and running
    Everything running aside from EA

--- main.cpp
+++ main.cpp
@@ -19,15 +19,12 @@
 
 
 #define crRand (double)rand()/RAND_MAX
-#define PI 3.1415296
-
 using namespace std;
-
-
 
 // Global Constants
 int numCities;
 int numMutations;
+int populationSize = 100;
 
 class city{
 public:
@@ -42,6 +39,7 @@ public:
     double fitness;
     double evalFitness(vector<city>);
     void initPolicy(vector<city>);
+    vector<city> mutatePolicy(vector<city>);
 };
 
 void initCities(vector<city>* allCities){
@@ -49,9 +47,9 @@ void initCities(vector<city>* allCities){
     cin>>numCities;
     cout<<endl;
     
-    //cout<<"How many mutation cycles would you like to complete?: ";
-    //cin>>numMutations;
-    //cout<<endl;
+    cout<<"How many mutation cycles would you like to complete?: ";
+    cin>>numMutations;
+    cout<<endl;
     
     for(int i=0; i<numCities; i++){
         city A;
@@ -61,7 +59,7 @@ void initCities(vector<city>* allCities){
 };
 
 void city::initCity(){
-    srand(time(0));
+    
     cityLocX = rand()% 100 + crRand - crRand;
     cityLocY = rand()% 100 + crRand - crRand;
 };
@@ -87,13 +85,11 @@ double policy::evalFitness(vector<city> cityOrder){
 };
 
 void initPolicies(vector<policy>* population, vector<city>* allCities){
-    
-    int numPolicies = 100;
     vector<city> policyChange = *allCities;
     srand(time(NULL));
     population->clear();
     
-    for (int i = 0; i < numPolicies; i++)
+    for (int i = 0; i < populationSize; i++)
     {
         for(int i=0; i<numCities; i++)
         {
@@ -120,13 +116,47 @@ void initPolicies(vector<policy>* population, vector<city>* allCities){
         A.initPolicy(policyChange);
         population->push_back(A);
     }
-    assert(population->size() == numPolicies);
+    assert(population->size() == populationSize);
+};
+
+vector<city> policy::mutatePolicy(vector<city> mutatePolicy){
+    vector<city> mutate;
+    int numChanges = numCities * .1;
+    for(int i = 0; i<numChanges; i++)
+    {
+    
+    }
+
+    return mutate;
+};
+
+vector<policy> replicatePop(vector<policy>* population){
+    // take in current population
+    vector<policy> mutatePop = *population;
+    assert(mutatePop.size() == populationSize);
+    
+    for(int i =0; i<populationSize; i++)
+    {
+        vector<city> mutatePolicy = population->at(i).cityOrder;
+        policy B;
+        B.initPolicy(mutatePolicy);
+        
+        
+        mutatePop.push_back(B);
+    }
+    
+    
+    cout<<"Mutated pop size: "<<mutatePop.size()<<endl;
+    assert(mutatePop.size() == populationSize * 2);
+    return mutatePop;
 };
 
 int main() {
+    srand(time(NULL));
     
     vector<policy> population;
     vector<city> allCities;
+    vector<policy> mutatedPopulation;
     
     // Initialize cities
     initCities(&allCities);
@@ -134,26 +164,18 @@ int main() {
     // Initialize policies
     initPolicies(&population, &allCities);
     
-    for(int i = 0; i < population.size(); i++ )
-    {
-        cout<<"Fitness: "<<population.at(i).fitness<<endl;
-        //cout<<"Fitness: "<<population.at(i).fitness
-        
-    }
-
-    for(int i = 0; i<numCities; i++)
-    {
-        cout<<"X location: "<<allCities.at(i).cityLocX<<endl;
-    }
+    
     
     // Start EA (for)
-    
+    for(int i = 0; i< numMutations; i++)
+    {
         // Replicate -> Mutate
-    
-        // Evaluate fitnesses
+        mutatedPopulation = replicatePop(&population);
     
         // Downselect -> Binary tournament
-    
+        
+        
+    }
     
 }
 
